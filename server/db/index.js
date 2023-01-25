4d9e80e2d4840a8f3c345b862099e3fd6b77fbef
Kevin Taylor <kevintaylor&84@gmail.com>
2023-01-25 16:19:22 -0500

    completed both Comment and post liks

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,17 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
-
-const User = require('./models/User')
+const db = require("./db");
+const Comments = require("./models/Comment");
+const Post_like = require('./models/Post_like');
+const User = require("./models/User");
 
 //associations could go here!
 
+
+
 module.exports = {
   db,
-  models: {
-    User,
-  },
-}
+  User,
+  Comments,
+  Post_like
+};
