aea01908efa505a67be667f890344f0589e0706f
Patrick Kenny <patrickmkenny91@gmail.com>
2023-01-25 16:19:54 -0600

    added associations

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,37 @@ const db = require('./db')
 
 const User = require('./models/User')
 
+// User.belongsToMany(User, {through: Friendship}) 
+// User.belongsToMany(Project, {throught: ProjectMembership})
+// User.hasMany(Report)
+// User.hasMany(Post)
+// 
+// Report.hasOne(User) 
+// 
+// Post.hasMany(Comment);
+// Post.hasMany(Like)
+// Post.belongsTo(User)
+// Post.belongsTo(Project);
+
+// PostLike.belongsTo(Post)
+
+// Comment.hasMany(CommentLike);
+// Comment.belongsTo(Post);
+
+// CommentLike.belongsTo(Comment);
+
+// Project.hasMany(ProjectTechnologies);
+// Project.belongsToMany(User, {through: ProjectMembership})
+// Project.hasMany(Post)
+
+// ProjectTechnologies.belongsTo(Project)
+
+
+
+
+
+
+
 //associations could go here!
 
 module.exports = {
