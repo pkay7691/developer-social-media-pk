551dd38f9678d7f7bc979292672a6d8e5c418d34
Camesha Goods <camesha.goods1@gmail.com>
2023-01-25 17:02:04 -0500

    added project_Membership model and corrected friendship model

--- server/db/models/Friendship.js
+++ server/db/models/Friendship.js
@@ -3,12 +3,12 @@ const db = require('../db');
 
 const Friendship = db.define('friendship',{
     status:{
-        type: Sequelize.STRING,
-        allowNull: false
+        type: Sequelize.ENUM,
+        allowNull: false,
+        values:['inReview', 'isFriend']
     },
     request_msg:{
-        type: Sequelize.ENUM,
-        values:['isFriend', 'isNotFriend']
+        type: Sequelize.TEXT   
     }
     //went with Enum because either can be true or validate
 })
