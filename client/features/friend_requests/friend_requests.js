3a3bd1d9d584d46c20beec330a8ee173c03f9843
Patrick Kenny <patrickmkenny91@gmail.com>
2023-02-06 15:01:18 -0600

    premerge

--- client/features/friend_requests/friend_requests.js
+++ client/features/friend_requests/friend_requests.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import { Table, TableBody, TableCell, TableHead, TableRow, Button } from "@mui/material";
-import { fetchFriendshipsById, selectFriendRequests } from "../friends/friendshipSlice";
+import { acceptFriendRequest, createFriendship, fetchFriendshipsById, selectFriendRequests, updateFriendship ,deleteFriendship } from "../friends/friendshipSlice";
 
 const FriendRequests = () => {
 
@@ -14,16 +14,49 @@ const FriendRequests = () => {
       dispatch(fetchFriendshipsById(loggedInUserId))
     },[])
 
+    const handleAcceptFriendRequest = (friendship) => {
+
+      const updatedFriendship = {
+        id: friendship.id,
+        userId: friendship.userId,
+        friendId: friendship.friendId,
+        friendName: friendship.friendName,
+        userName: friendship.userName,
+        status: 'isFriend',
+        compositeId: `${friendship.userId}&${friendship.friendId} `
+      }
+      const newFriendship = {
+        userId: friendship.friendId,
+        friendId: friendship.userId,
+        friendName: friendship.userName,
+        userName: friendship.friendName,
+        status: 'isFriend',
+        compositeId: `${friendship.friendId}&${friendship.userId} `
+      }
+      dispatch(updateFriendship(updatedFriendship))
+      dispatch(createFriendship(newFriendship))
+    }
+
+    const handleRejectFriendRequest = (friendship) => {
+      dispatch(deleteFriendship(friendship.id))
+    }
+
 
 
     //this will return a table of all users
     return (
         <div>
+          {friendRequests && friendRequests.length ? friendRequests.map((request) => <div key={`request-${request.id}`}>
+          <Link to={`/user/${request.friendId}`}>{request.friendName}</Link>
+            <button  onClick={(e) => handleAcceptFriendRequest(request)}>Accept</button>
+            <button  onClick={(e) => handleRejectFriendRequest(request)}>Reject</button>
+          </div>
+         ) : null}
   
    
 
         </div>
-    );
+    )
 
 }
 
