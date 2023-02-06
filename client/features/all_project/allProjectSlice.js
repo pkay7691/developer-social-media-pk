f46a30518b4f100e2c1400070b176a02e166dded
Jeff Zhang <jeffzhang@Jeffs-MacBook-Pro.local>
2023-02-06 12:21:00 -0500

    finished all projects page

---
+++ client/features/all_project/allProjectSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAllProjectAsync = createAsyncThunk('project', async () => {
+    try {
+        const { data } = await axios.get('/api/project');
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
+const projectsSlice = createSlice({
+    name: 'projects',
+    initialState: {},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchAllProjectAsync.fulfilled, (state, action) => {
+            return action.payload
+        })
+    }
+})
+
+export const selectProjects = (state, action) => {
+    return state.projects
+}
+
+export default projectsSlice.reducer;
