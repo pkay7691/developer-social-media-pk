3f03894534de08055c94cc64a2b50b73a5d5d5ac
Kevin <kewilliams2017@hotmail.com>
2023-02-13 17:50:03 -0500

    finished customizing auth

--- client/app/theme.js
+++ client/app/theme.js
@@ -5,13 +5,34 @@ import { purple } from '@mui/material/colors';
 const theme = createTheme({
   palette: {
     primary: {
-      // Purple and green play nicely together.
-      main: purple[500],
+      // Dark blue
+      main: '#023c40',
     },
     secondary: {
       // This is green.A700 as hex.
-      main: '#11cb5f',
+      main: '#93b7be',
     },
+    tertiary: {
+      main: '#ff9fb2',
+    },
+    //if mode is dark, then the background is dark
+    mode: 'dark',
+    //if mode is light, then the background is light
+    mode: 'light',
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
   },
 });
 
