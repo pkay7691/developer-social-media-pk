98f12ad1dc41217b23bd94bb279b3cc90803857d
Jeff Zhang <jeffzhang@jeffs-mbp.myfiosgateway.com>
2023-02-14 20:09:06 -0500

    updated landing and friend requests

--- client/features/friend_requests/friend_requests.js
+++ client/features/friend_requests/friend_requests.js
@@ -2,69 +2,86 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
-import { Table, TableBody, TableCell, TableHead, TableRow, Button } from "@mui/material";
-import { acceptFriendRequest, createFriendship, fetchFriendshipsById, selectFriendRequests, updateFriendship ,deleteFriendship } from "../friends/friendshipSlice";
+import { Table, TableBody, TableCell, TableHead, TableRow, Button, Stack } from "@mui/material";
+import { acceptFriendRequest, createFriendship, fetchFriendshipsById, selectFriendRequests, updateFriendship, deleteFriendship } from "../friends/friendshipSlice";
 
 const FriendRequests = () => {
 
   const loggedInUserId = useSelector((state) => state.auth.me.id);
   const friendRequests = useSelector(selectFriendRequests)
-    const dispatch = useDispatch();
-    const [renderFriendRequests, setRenderFriendRequests] = useState(false)
+  const dispatch = useDispatch();
+  const [renderFriendRequests, setRenderFriendRequests] = useState(false)
 
-    useEffect(() => {
-      dispatch(fetchFriendshipsById(loggedInUserId))
-    },[dispatch, renderFriendRequests])
+  useEffect(() => {
+    dispatch(fetchFriendshipsById(loggedInUserId))
+  }, [dispatch, renderFriendRequests])
 
-    const handleAcceptFriendRequest = (friendship) => {
+  const handleAcceptFriendRequest = (friendship) => {
 
-      const updatedFriendship = {
-        id: friendship.id,
-        userId: friendship.userId,
-        friendId: friendship.friendId,
-        friendName: friendship.friendName,
-        userName: friendship.userName,
-        status: 'isFriend',
-        compositeId: `${friendship.userId}&${friendship.friendId} `
-      }
-      const newFriendship = {
-        userId: friendship.friendId,
-        friendId: friendship.userId,
-        friendName: friendship.userName,
-        userName: friendship.friendName,
-        status: 'isFriend',
-        compositeId: `${friendship.friendId}&${friendship.userId} `
-      }
-      dispatch(updateFriendship(updatedFriendship))
-      dispatch(createFriendship(newFriendship))
-      setRenderFriendRequests(!renderFriendRequests)
+    const updatedFriendship = {
+      id: friendship.id,
+      userId: friendship.userId,
+      friendId: friendship.friendId,
+      friendName: friendship.friendName,
+      userName: friendship.userName,
+      status: 'isFriend',
+      compositeId: `${friendship.userId}&${friendship.friendId} `
     }
-
-    const handleRejectFriendRequest = (friendship) => {
-      console.log("hello")
-      dispatch(deleteFriendship(friendship.id))
-      console.log(renderFriendRequests)
-      setRenderFriendRequests(!renderFriendRequests)
-      console.log(renderFriendRequests)
+    const newFriendship = {
+      userId: friendship.friendId,
+      friendId: friendship.userId,
+      friendName: friendship.userName,
+      userName: friendship.friendName,
+      status: 'isFriend',
+      compositeId: `${friendship.friendId}&${friendship.userId} `
     }
+    dispatch(updateFriendship(updatedFriendship))
+    dispatch(createFriendship(newFriendship))
+    setRenderFriendRequests(!renderFriendRequests)
+  }
 
-// TODO: Inconsistent rendering of requests
+  const handleRejectFriendRequest = (friendship) => {
+    console.log("hello")
+    dispatch(deleteFriendship(friendship.id))
+    console.log(renderFriendRequests)
+    setRenderFriendRequests(!renderFriendRequests)
+    console.log(renderFriendRequests)
+  }
 
-    //this will return a table of all users
-    return (
-        <div>
-          {friendRequests && friendRequests.length ? friendRequests.map((request) => <div key={`request-${request.id}`}>
-          <Link to={`/user/${request.friendId}`}>{request.friendName}</Link>
-            <button  onClick={(e) => handleAcceptFriendRequest(request)}>Accept</button>
-            <button  onClick={(e) => handleRejectFriendRequest(request)}>Reject</button>
-          </div>
-         ) : null}
-  
-   
+  // TODO: Inconsistent rendering of requests
 
-        </div>
-    )
-
+  //this will return a table of all users
+  return (
+    <Stack
+    spacing='1rem'
+    width={{base: '90%', md: '500px'}}
+    margin='auto'
+    height='100vh'
+    >
+      <Typography variant="h2" color='primary' align="center">Friend Requests</Typography>
+      <Table size="medium">
+      <TableHead>
+        <TableRow>
+          <TableCell align="left">Friend</TableCell>
+          <TableCell align="right">Accept/Decline</TableCell>
+        </TableRow>
+      </TableHead>
+      </Table>
+      <Table size="medium">
+      <TableBody>
+      {friendRequests && friendRequests.length ? friendRequests.map((request) => 
+      <TableRow key={`request-${request.id}`}>
+        <TableCell align="left"><Link to={`/user/${request.friendId}`}>{request.friendName}</Link></TableCell>
+        <TableCell align='right'>
+        <Button variant="contained" onClick={(e) => handleAcceptFriendRequest(request)}>Accept</Button>
+        <Button variant="contained" onClick={(e) => handleRejectFriendRequest(request)}>Reject</Button>
+        </TableCell>
+      </TableRow>
+      ) : null}
+      </TableBody>
+      </Table>
+    </Stack>
+  )
 }
 
 export default FriendRequests;
--- client/features/landing_page/landing_page.js
+++ client/features/landing_page/landing_page.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { createTheme, CssBaseline, ThemeProvider, Typography, Container, Collapse, IconButton, Grid, Card, CardContent, CardMedia, CardActionArea, Stack } from '@mui/material'
+import {
+    createTheme,
+    CssBaseline,
+    ThemeProvider,
+    Typography,
+    Container,
+    Collapse,
+    IconButton,
+    Grid,
+    Card,
+    CardContent,
+    CardMedia,
+    CardActionArea,
+    Stack,
+    Avatar,
+    AvatarGroup,
+    Box,
+    CardActions,
+    Button,
+} from '@mui/material'
 
 
 const LandingPage = () => {
@@ -11,50 +30,170 @@ const LandingPage = () => {
 
     return (
         <div>
-            <Container maxWidth='large' minHeight='100vh' >
-                <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={100} align='center'>
-                    <Typography variant='h2' color='textPrimary'>Landing Page</Typography>
+            <Container maxWidth='large' minheight='100vh' >
+                <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedheight={100} align='center'>
+                    <img src='logo.png' alt='logo' />
                 </Collapse>
-                <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={100} align='center'>
-                    <Stack sx={{ pt: '1' }} width={{ base: '90%', md: '600px' }} direction='row' spacing={10} justifyContent='center' marginTop={10}>
+                <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedheight={100} align='center'>
+                    <Stack sx={{ pt: '1' }} width={{ base: '100%', md: '900px' }} direction='row' spacing={20} justifyContent='center' marginTop={5}>
                         <Card sx={{ maxWidth: 600 }}>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="100vh"
-                                    image=""
-                                    alt=""
-                                />
-                                <CardContent >
-                                    <Typography gutterBottom variant="h5" component="div" align='center'>
-                                        About Us
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur. Lobortis mattis aliquam faucibus purus. Morbi blandit cursus risus at ultrices. Accumsan tortor posuere ac ut consequat semper viverra nam libero. In aliquam sem fringilla ut. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Tempus imperdiet nulla malesuada pellentesque elit eget. Scelerisque felis imperdiet proin fermentum leo vel orci. Tempus iaculis urna id volutpat lacus. Porttitor lacus luctus accumsan tortor posuere ac. Porta non pulvinar neque laoreet. Velit sed ullamcorper morbi tincidunt ornare massa eget.
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
+                            <CardContent >
+                                <Typography gutterBottom variant="h5" component="div" align='center'>
+                                    About Us
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur. Lobortis mattis aliquam faucibus purus. Morbi blandit cursus risus at ultrices. Accumsan tortor posuere ac ut consequat semper viverra nam libero. In aliquam sem fringilla ut. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Tempus imperdiet nulla malesuada pellentesque elit eget.
+                                </Typography>
+                            </CardContent>
                         </Card>
                         <Card sx={{ maxWidth: 600 }}>
                             <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="100vh"
-                                    image=""
-                                    alt=""
-                                />
                                 <CardContent >
                                     <Typography gutterBottom variant="h5" component="div" align='center'>
                                         Objective
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur. Lobortis mattis aliquam faucibus purus. Morbi blandit cursus risus at ultrices. Accumsan tortor posuere ac ut consequat semper viverra nam libero. In aliquam sem fringilla ut. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Tempus imperdiet nulla malesuada pellentesque elit eget. Scelerisque felis imperdiet proin fermentum leo vel orci. Tempus iaculis urna id volutpat lacus. Porttitor lacus luctus accumsan tortor posuere ac. Porta non pulvinar neque laoreet. Velit sed ullamcorper morbi tincidunt ornare massa eget.
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur. Lobortis mattis aliquam faucibus purus. Morbi blandit cursus risus at ultrices. Accumsan tortor posuere ac ut consequat semper viverra nam libero. In aliquam sem fringilla ut. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Tempus imperdiet nulla malesuada pellentesque elit eget.
                                     </Typography>
                                 </CardContent>
                             </CardActionArea>
                         </Card>
                     </Stack>
                 </Collapse>
+            </Container>
+            <Container sx={{ py: 9 }} maxWidth="lg">
+                <Grid container spacing={2} columns={10}>
+                    <Grid item xs={12} sm={6} md={2}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column', border: 'hidden' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    p: '30px',
+                                    mt: '15%',
+                                    borderRadius: '50%'
+                                }}
+                                crossOrigin="anonymous"
+                                image='jeffzhang.jpeg'
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>
+                                    Jeff Zhang
+                                </Typography>
+                                <Typography variant='h6' align='center'>Role: Developer</Typography>
+                                <Typography>
+                                "I am an aspiring developer looking to takeover ChatGPT and love to play basketball in my free time "
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={2}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column', border: 'hidden' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    p: '30px',
+                                    mt: '15%',
+                                    borderRadius: '50%'
+                                }}
+                                crossOrigin="anonymous"
+                                image="patrickkenny.jpg"
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>
+                                    Patrick Kenny
+                                </Typography>
+                                <Typography variant='h6' align='center'>Role: Developer</Typography>
+                                <Typography>
+                                "Chicago based programmer chasing his dream of being employed again"
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={2}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column', border: 'hidden' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    p: '30px',
+                                    mt: '15%',
+                                    borderRadius: '50%'
+                                }}
+                                crossOrigin="anonymous"
+                                image="kevintaylor2.jpg"
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>
+                                    Kevin Taylor
+                                </Typography>
+                                <Typography variant='h6' align='center'>Role: Developer</Typography>
+                                <Typography>
+                                "retired marine, future Software Engineer. Favorite quote: hackers of the world unite "
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={2}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column', border: 'hidden' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    p: '30px',
+                                    mt: '15%',
+                                    borderRadius: '50%'
+                                }}
+                                crossOrigin="anonymous"
+                                image="kevinwilliams.jpeg"
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>
+                                    Kevin Williams
+                                </Typography>
+                                <Typography variant='h6' align='center'>Role: Developer</Typography>
+                                <Typography>
+                                "I enjoy programming.  I consider myself a life long learner.  Sidenote: Tarantulas are actually pretty cool."
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={2}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column', border: 'hidden' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    p: '30px',
+                                    mt: '15%',
+                                    borderRadius: '50%'
+                                }}
+                                crossOrigin="anonymous"
+                                image="cameshagoods.jpg"
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>
+                                    Camesha Goods
+                                </Typography>
+                                <Typography variant='h6' align='center'>Role: Developer</Typography>
+                                <Typography>
+                                “I haven't spoken to my mom in years. I didn't want to interrupt her.”
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     )
